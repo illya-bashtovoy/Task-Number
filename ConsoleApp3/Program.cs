29d65f0b80ce4cf502ba62db43600dd5979236e0
illya-bashtovoy <bashtoff@gmail.com>
2023-03-12 14:27:42 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Task task = Task.Run(() =>
+            {
+                for (int i = 2; i <= 1000; i++)
+                {
+                    bool isPrime = true;
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            });
+
+            // Очікуємо завершення задачі
+            task.Wait();
+
+            Console.WriteLine("Усі прості числа від 0 до 1000 відображено.");
+            Console.ReadKey();
+        }
+    }
+}
